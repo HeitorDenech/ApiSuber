0ab958e668f9b0da36dac9c5243803b799a45e7b
https://github.com/HeitorDenech <heitor.denech@gmail.com>
2024-04-16 17:55:33 -0300

    suber-final

---
+++ front/script2.js
@@ -0,0 +1,60 @@
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+})
+
+// -----------header-------------
+
+// sr.reveal('#logo', {delay:100, origin: 'left'});
+
+// sr.reveal('.navbar-text', {delay:100, origin: 'right'});
+
+sr.reveal('.cabecario-text', {delay:100, origin: 'left'});
+
+sr.reveal('.img-nav-main', {delay:100, origin: 'right'});
+
+// -----------sobre------------
+
+sr.reveal('#carro-uber', {delay:100, origin: 'top'});
+
+sr.reveal('#text-sobre-h1', {delay:100, origin: 'top'});
+
+sr.reveal('#text-sobre-p', {delay:100, origin: 'top'});
+
+sr.reveal('#botao-sobre', {delay:100, origin: 'top'});
+
+// ------------cadastro--------------
+
+sr.reveal('#img-cadastro', {delay:100, origin: 'left'});
+
+sr.reveal('#text-cadastro-h1', {delay:100, origin: 'right'});
+
+sr.reveal('#text-cadastro-p', {delay:100, origin: 'right'});
+
+sr.reveal('#title', {delay:100, origin: 'right'});
+
+sr.reveal('#description', {delay:100, origin: 'right'});
+
+sr.reveal('.botao-cadastro', {delay:100, origin: 'right'});
+
+// -------------servicos-------------
+
+sr.reveal('#img-servico', {delay:100, origin: 'top'});
+
+sr.reveal('#text-servico-h1', {delay:100, origin: 'top'});
+
+sr.reveal('#text-servico-p1', {delay:100, origin: 'top'});
+
+sr.reveal('#text-servico-p2', {delay:100, origin: 'top'});
+
+// ------------footer-------------
+
+// sr.reveal('#logo-footer', {delay:100, origin: 'left'});
+
+// sr.reveal('#text-footer', {delay:100, origin: 'top'});
+
+// sr.reveal('.icons', {delay:100, origin: 'right'});
+
+// sr.reveal('.container-download', {delay:100, origin: 'top'});
