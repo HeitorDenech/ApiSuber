eb54c12e6501a43ebdb419d0dae651827f6f4f55
https://github.com/HeitorDenech <heitor.denech@gmail.com>
2024-04-18 18:02:24 -0300

    api-suber-atualizado

--- front/scrpt.js
+++ front/scrpt.js
@@ -1,30 +1,53 @@
 let button = document.getElementById("handleSubmit");
  
 button.onclick = async function () {
-    let title = document.getElementById("title").value;
-    let description = document.getElementById("description").value;
-    let data = { title, description }
- 
-    const response = await fetch('http://localhost:3000/api/store/task', {
-        method: "POST",
-        headers: { "Content-type": "application/json;charset=UTF-8" },
-        body: JSON.stringify(data)
-    });
- 
-    let content = await response.json();
- 
-    // if (content.sucess) {
-    //     alert("Sucesso")
-    // } else {
-    //     alert('Você foi cadastrado com sucesso!');
-        
-    // }
-    swal({
-        title: "Parabéns!",
-        text: "Você foi cadastrado com sucesso!",
-        icon: "success",
-      });
+    let nome = document.getElementById("title").value;
+    let email = document.getElementById("description").value;
 
+    let regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    let results = regexEmail.test(email);
+    //console.log(results);
+    
+
+    if(nome === "" && email === "") {
+        swal({
+            title: "Preencha os campos!",
+            icon: "error",
+        });
+    } else if(nome === "" || nome === null) {
+        swal({
+            title: "Verifique seu nome!",
+            icon: "error",
+        });
+    } else if(email === "" || !results) {
+        swal({
+            title: "Verifique seu email!",
+            icon: "error",
+        });
+    } else {
+        let data = { nome, email };
+
+        const response = await fetch('http://localhost:3000/api/store/task', {
+            method: "POST",
+            headers: { "Content-type": "application/json;charset=UTF-8" },
+            body: JSON.stringify(data)
+        });
+    
+        let content = await response.json();
+    
+        if (content.success) {
+            swal({
+                title: "Parabéns!",
+                text: "Você foi cadastrado com sucesso!",
+                icon: "success",
+            });
+        } else {
+            swal({
+                title: "Email já cadastrado",
+                icon: "error",
+            });
+        }
+    }
 }
 
 
